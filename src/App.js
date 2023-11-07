5b7e2a63de8fad80bc172b0913f97a0e1187190d
JASWANT2001 <k.jaswant2305@gmail.com>
2023-11-07 09:25:50 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Message from "./Message";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message place = "Earth" place1 = "Say Hiee!"></Message>
+      <Message place = "Moon"></Message>
+      <Message place = "Jupiter"></Message>
     </div>
   );
 }
---
+++ src/Message.jsx
@@ -0,0 +1,6 @@
+function Message({place,place1}) {
+    return <div>
+        <h1>Hello {place}! {place1}</h1>
+    </div>
+}
+export default Message;
