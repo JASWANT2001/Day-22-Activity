b9c67df67a4bc29f70adac16c36e795278061a7d
JASWANT2001 <k.jaswant2305@gmail.com>
2023-11-08 18:04:13 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import Message from "./Message";
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <Message place = "Earth" place1 = "Say Hiee!"></Message>
-      <Message place = "Moon"></Message>
-      <Message place = "Jupiter"></Message>
+    <section className="pricing py-5">
+    <div className="container">
+      <div className="row">
+      
+      <Message plan = "Free" user = "Single user" price = "$0/" storage = "5GB storage"></Message>
+      <Message plan = "Plus"  user = "5 users" price = "$9/" storage = "50GB storage"></Message>
+      <Message plan = "Pro"  user = "Unlimited users" price = "$49/" storage = "150GB storage"></Message>
+      
     </div>
+    </div>
+    </section>
+   
   );
 }
 
